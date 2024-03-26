b45f431264c0831a3d1d838913b1b2baf40451d2
Yash Verma <yash.verma33334@gmail.com>
2024-03-26 15:47:22 +0530

    commit 4

--- pages/components/Hotel.jsx
+++ pages/components/Hotel.jsx
@@ -1,35 +1,29 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React from 'react';
 
 const Hotel = (props) => {
-  const hotel=props.hotel;
-  const router=useRouter();
-  return (
-    <div onClick={()=>{router.push('/Hotels/'+hotel._id)}} className='flex border-red-400 w-full h-96 p-4 border-2 border rounded-lg'>
-  
-   <div className='flex'>
-    <img src={hotel.banner}
-     width={400}
-     height={30}
-    ></img>
-    <div className='p-3 gap-4'>
-        <p className='font-bold'>
-            {hotel.name}
-        </p>
-<p>{hotel.description}</p>
-    <p>Facilities:-</p>
-    <div className='flex gap-2'>
-   <button className='px-6 py-3 rounded-md bg-blue-200'>Price:{hotel.price}</button>
-   <button className='px-6 py-3 rounded-md bg-blue-200'>See Details</button>
-    
-    </div>
-    </div>
-    
-    </div> 
+  const hotel = props.hotel;
+  const router = useRouter();
 
+  return (
+    <div onClick={() => { router.push('/Hotels/' + hotel._id) }} className='flex border-red-400 w-full h-96 p-4 border-2 border rounded-lg'>
+      <div className='flex'>
+        {hotel?.banner && (
+          <Image src={hotel.banner} width={400} height={300} alt="Hotel Banner" />
+        )}
+        <div className='p-3 gap-4'>
+          <p className='font-bold'>{hotel.name}</p>
+          <p>{hotel.description}</p>
+          <p>Facilities:-</p>
+          <div className='flex gap-2'>
+            <button className='px-6 py-3 rounded-md bg-blue-200'>Price: {hotel.price}</button>
+            <button className='px-6 py-3 rounded-md bg-blue-200'>See Details</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hotel
+export default Hotel;
